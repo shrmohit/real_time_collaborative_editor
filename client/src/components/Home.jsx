4efe304a3ea87d619f2497a582808bd1e588147c
mohit sharma <mohitsharma7258@gmail.com>
2025-05-12 15:11:12 +0530

    home

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,42 @@
+const Home = () => {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center bg-[#1c1e29]">
+      <div className="w-full max-w-xl px-4">
+        <div className="bg-gray-700 shadow-md rounded-lg p-4">
+          <div className="bg-gray-900 p-6 text-center rounded-lg">
+            <h1 className="text-white">Editor</h1>
+            <h4 className="text-white text-2xl font-semibold mb-4">
+              Enter the ROOM ID
+            </h4>
+
+            <div className="mb-3">
+              <input
+                type="text"
+                placeholder="ROOM ID"
+                className="w-full p-3 rounded-md bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-400"
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                placeholder="USERNAME"
+                className="w-full p-3 rounded-md bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-400"
+              />
+            </div>
+            <button className="w-full cursor-pointer bg-green-500 hover:bg-green-600 text-white text-lg py-3 rounded-md transition duration-300">
+              JOIN
+            </button>
+            <p className="mt-4 text-white">
+              Don't have a room ID? create{" "}
+              <span className="text-green-400 hover:underline cursor-pointer">
+                New Room
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
